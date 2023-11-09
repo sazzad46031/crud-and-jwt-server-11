a6d0dd2938edc224542988802b5d5a6f6091ac9b
sazzad46031 <git config --global user.email sazzadmgbs@gmail.com>
2023-11-09 13:33:41 +0600

    post endpoint added

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb'); 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
 
 app.use(cors());
 app.use(express.json());
 
-// crudMaster
-// jcGUoaEJVsyuuCSe
+
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.no3nfin.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,12 +27,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodCollection = client.db('foodDB').collection('food')
+    
+    app.get('/foods', async(req,res)=>{
+        const cursor = foodCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/foods/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await foodCollection.findOne(query)
+        res.send(result)
+    })
+
+    app.post('/foods',async(req,res)=>{
+        const newFood = req.body
+        console.log(newFood)
+        const result = await foodCollection.insertOne(newFood)
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
