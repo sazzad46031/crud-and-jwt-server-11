d81af7e961e053b489839cffc288f80206873026
sazzad46031 <git config --global user.email sazzadmgbs@gmail.com>
2023-11-09 18:06:57 +0600

    delete endponit added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
     await client.connect();
 
     const foodCollection = client.db('foodDB').collection('food')
-    
+    const foodRequestCollection = client.db('foodrequestDB').collection('foodrequest')
     app.get('/foods', async(req,res)=>{
         const cursor = foodCollection.find()
         const result = await cursor.toArray()
@@ -49,7 +49,18 @@ async function run() {
         const result = await foodCollection.insertOne(newFood)
         res.send(result)
     })
-
+    app.post('/request',async(req,res)=>{
+        const newRequest = req.body
+        console.log(newRequest)
+        const result = await foodRequestCollection.insertOne(newRequest)
+        res.send(result)
+    })
+    app.delete('/foods/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await foodCollection.deleteOne(query)
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
